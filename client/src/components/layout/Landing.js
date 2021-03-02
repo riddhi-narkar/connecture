f620c169a1558d1145816b8b709dbb756756238d
Riddhi Narkar <62413157+grey-hat2003@users.noreply.github.com>
2021-03-02 22:22:03 +0530

    Changed to "Apsit Hub" to "Connecture"

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Landing = () => {
   return (
     <div>
-      <h1>Apsit Hub</h1>
+      <h1>Connecture</h1>
       <div>
         <Link to="/register">Sign Up</Link>
         <Link to="/login">Login</Link>

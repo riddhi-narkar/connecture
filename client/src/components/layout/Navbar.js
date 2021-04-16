dbb3b5153da2eb218aee88fe15c0e4eafe80827e
Shauryan Singh <shauryan23singh@gmail.com>
2021-04-16 21:06:31 +0530

    Add Dyamic Navbar Logout Link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { logout } from '../../actions/auth';
 
-export const Navbar = () => {
-  return (
-    <nav>
-      <h1>
-        <Link to="/">Connecture</Link>
-      </h1>
-      <ul>
+export const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
+  const authLinks = (
+    <ul>
         <li>
-          <Link to="/register">Register</Link>
+          <a onClick={logout} href='#!'>Logut</a>
+        </li> 
+      </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+        <li>
+          <Link to ="/register">Register</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to ="/login">Login</Link>
         </li>
       </ul>
+  );
+  
+    return (
+    <nav>
+      <h1>
+        <Link to ="/"> Connecture </Link>
+      </h1>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+ });
+
+export default connect(mapStateToProps, { logout })(Navbar);

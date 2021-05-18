9c35e2faeeab8e7128c06ed3559e564aaf62a6b8
grey-hat2003 <62413157+grey-hat2003@users.noreply.github.com>
2021-05-18 07:56:31 +0530

    updated models

--- models/Profile.js
+++ models/Profile.js
@@ -5,12 +5,6 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
   },
-  company: {
-    type: String
-  },
-  website: {
-    type: String
-  },
   location: {
     type: String
   },
@@ -28,35 +22,7 @@ const ProfileSchema = new mongoose.Schema({
   githubusername: {
     type: String
   },
-  experience: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ],
+
   education: [
     {
       school: {
@@ -88,21 +54,12 @@ const ProfileSchema = new mongoose.Schema({
     }
   ],
   social: {
-    youtube: {
-      type: String
-    },
     twitter: {
-      type: String
-    },
-    facebook: {
       type: String
     },
     linkedin: {
       type: String
     },
-    instagram: {
-      type: String
-    }
   },
   date: {
     type: Date,
